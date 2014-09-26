d0ec3e0c0e1f80bba4bc1f29a4b09d09c9327f7c
patrickczeczko <patrickczeczko@10.11.85.94>
2014-09-26 10:38:59 -0600

    unsorted list created

--- src/lists/DynamicList.java
+++ src/lists/DynamicList.java
@@ -28,31 +28,10 @@ public class DynamicList {
 
 	}
 
-	/**
-	 * Returns the index of the element with specified key. If there is no
-	 * element with the specified key, this method will return -1. (If there is
-	 * more than one element with specified key, the first with the required key
-	 * is returned.)
-	 * 
-	 * @param key
-	 *            the key of the element we are searching for.
-	 * @return the index of the element with specified key or -1 if there is no
-	 *         element with specified key.
-	 */
 	protected void getIndex() {
 
 	}
 
-	/**
-	 * Does a linear search to find the element with the specified key. If there
-	 * is no element with the specified key this method will return null. (If
-	 * there is more than one element with specified key, the first with the
-	 * required key is returned.)
-	 * 
-	 * @param key
-	 *            the key of the element we are searching for.
-	 * @return the element in this list that has specified key.
-	 */
 	public void find() {
 
 	}
